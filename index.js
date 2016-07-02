b3fc6d85489b16a1419f8065bf1089894da09a72
modernator <rico345100@gmail.com>
2016-07-02 20:12:18 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,149 @@
+"use strict";
+const EventEmitter = require('events').EventEmitter;
+const util = require('util');
+const fs = require('fs');
+const path = require('path');
+
+const files = {};
+const CHUNK_SIZE = 524288;
+const MAX_BUFFER_SIZE = 10485760;
+
+function mkdirSyncRecursively(dir, mode) {
+    try {
+        var result = fs.mkdirSync(dir, mode);
+    }
+    catch(e) {
+        if(e.code === 'ENOENT') {
+            mkdirSyncRecursively(path.dirname(dir), mode);  // if does not exists, create all parents recursively
+            mkdirSyncRecursively(dir, mode);   // retry
+        }
+    }
+}
+
+function SocketIOFile(socket, options) {
+    var self = this;
+    options = options || {};
+
+	this.socket = socket;
+    this.uploadDir = '';
+
+    // create upload dir if not exists
+    if(options.uploadDir) {
+        let dir = options.uploadDir;
+
+        try {
+            fs.accessSync(dir, fs.F_OK);
+        }
+        catch(e) {
+            // create directory if not exists
+            mkdirSyncRecursively(dir, '0755');
+        }
+
+        this.uploadDir = dir;
+    }
+
+	this.socket.on('socket.io-file::start', (data) => {
+		let fileName = data.name;
+        
+		// Create a new Entry in The Files Variable
+		files[fileName] = { 
+            size: data.size,
+            data: '',
+            uploaded: 0
+        };
+
+		let stream = 0;
+
+		try {
+            let stat = fs.statSync(`${this.uploadDir}/${fileName}`);
+
+            if(stat.isFile()) {
+                files[fileName].uploaded = stat.size;
+                stream = stat.size / CHUNK_SIZE;
+            }
+        }
+		// It's a New File
+        catch(e) {}
+
+        self.emit('start', data);
+
+        fs.open(`${this.uploadDir}/${fileName}`, 'a', '0755', (err, fd) => {
+            if(err) {
+                throw err;
+            }
+            else {
+                files[fileName].fd = fd;	 //We store the file handler so we can write to it later
+
+                const streamObj = { 
+					stream,
+                    size: files[fileName].size,
+					uploaded: 0,
+					percent: 0
+				};
+
+                this.emit('stream', streamObj);
+                socket.emit('socket.io-file::stream', streamObj);
+            }
+        });
+	});
+
+	this.socket.on('socket.io-file::stream', (data) => {
+		let fileName = data.name;
+        files[fileName].uploaded += data.data.length;
+        files[fileName].data += data.data;
+
+		// on fully uploaded
+        if(files[fileName].uploaded == files[fileName].size) {
+            fs.write(files[fileName].fd, files[fileName].data, null, 'Binary', (err, writen) => {
+                const streamObj = { 
+					stream,
+                    size: files[fileName].size,
+					uploaded: files[fileName].size,
+					percent: (files[fileName].uploaded / files[fileName].size) * 100
+				};
+
+                this.emit('stream', streamObj);
+                this.emit('complete', {});
+
+				socket.emit('socket.io-file::stream', streamObj);
+                socket.emit('socket.io-file::complete', {});
+
+                delete files[fileName];
+            });
+        }
+		// on reaches buffer limit
+        else if(files[fileName].data.length > MAX_BUFFER_SIZE) {
+            fs.write(files[fileName].fd, files[fileName].data, null, 'Binary', (err, writen) => {
+                files[fileName].data = '';		// clear the buffer
+
+                var stream = files[fileName].uploaded / CHUNK_SIZE;
+
+                const streamObj = { 
+					stream,
+                    size: files[fileName].size,
+					uploaded: files[fileName].uploaded,
+					percent: (files[fileName].uploaded / files[fileName].size) * 100
+				};
+
+                this.emit('stream', streamObj);
+                socket.emit('socket.io-file::stream', streamObj);
+            });
+		}
+        else{
+            var stream = files[fileName].uploaded / CHUNK_SIZE;
+            
+            const streamObj = { 
+                stream,
+                size: files[fileName].size,
+				uploaded: files[fileName].uploaded,
+				percent: (files[fileName].uploaded / files[fileName].size) * 100
+            };
+            
+            this.emit('stream', streamObj);
+            socket.emit('socket.io-file::stream', streamObj);
+        }
+	});
+}
+util.inherits(SocketIOFile, EventEmitter);
+
+module.exports = SocketIOFile;
---
+++ readme.md
@@ -0,0 +1,114 @@
+# Socket.io-file
+
+Socket.io-file is module for uploading file via Socket.io.
+
+## Example
+### Server side
+
+```javascript
+const SocketIOFile = require('socket.io-file');
+
+io.on('connection', (socket) => {
+	console.log('connected: ' + socket.id);
+
+	var uploader = new SocketIOFile(socket, {
+		uploadDir: 'data/music'
+	});
+
+	uploader.on('start', (fileInfo) => {
+		console.log('Upload started');
+	});
+	uploader.on('stream', (data) => {
+		console.log('Streaming... ' + data.uploaded + ' / ' + data.size);
+	});
+	uploader.on('complete', () => {
+		console.log('Completed!');
+	});
+});
+```
+
+### Client side
+
+#### HTML
+```html
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Socket File Upload</title>
+</head>
+<body>
+	<div id="UploadBox">
+		<h2>File Uploader</h2>
+		<span id="UploadArea">
+			<label for="FileBox">Choose A File:</label>
+			<input type="file" id="FileBox" />
+			<br />
+
+			<button type="button" id="UploadButton">Upload</button>
+		</span>
+	</div>
+
+	<script src="/socket.io.js"></script>
+	<script src="/socket.io-file-client.js"></script>
+	<script src="/alter.js"></script>
+</body>
+</html>
+```
+
+#### JavaScript
+```javascript
+var socket = io('http://localhost:3000');
+
+window.addEventListener('load', function() {
+	var socketIOFile = new SocketIOFileClient(socket);
+
+	socketIOFile.on('start', function() {
+		console.log('File uploading staring...');
+	});
+
+	socketIOFile.on('stream', function(data) {
+		//console.log('SocketIOFileClient: Client streaming... ' + (Math.round(data.percent * 100)/100) + '%');
+		console.log('SocketIOFileClient: Client streaming... ' + data.uploaded + ' / ' + data.size);
+	});
+
+	socketIOFile.on('complete', function() {
+		console.log('File Uploaded Successfully!');
+	});
+
+	document.getElementById('UploadButton').addEventListener('click', function() {
+		var file = document.getElementById('FileBox').files[0];
+		socketIOFile.upload(file);
+	});
+});
+```
+
+
+## API
+### constructor SocketIOFile(socket, options)
+
+Create new SocketIOFile object. This object automatically handles all file uploads from client via Socket.io.
+
+Options are:
+* uploadDir: Path for uploading file. Directory can be recursive, like 'user/data/music'.
+
+
+### Event.start(fileInfo)
+This event fires when upload begins, sending fileInfo object as first parameter received from Client before sending it. fileInfo object contains: name(file's name) and size(file's size).
+
+### Event.stream(streamData)
+This event fires when client sending the data from server, and server receives it. streamData is object that contains:
+* Object stream: Internally, this module merge the data from client until file is all uploaded. This stream is part of file that client keep sending it.
+* Number size: Total file size.
+* Number uploaded: Amount of uploaded.
+* Number percent: Percentage of how much uploaded
+
+### Event.complete()
+This event fires when upload completed.
+
+
+## Browser Supports
+This module uses FileReader API, so latest browser is required.
+
+
+Please check the Client side module too. Link: [socket.io-file-client
+](https://github.com/rico345100/socket.io-file)
