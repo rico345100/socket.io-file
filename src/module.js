2d21e028429acfd0fc8cb569e727cb07ce007bf5
modernator <rico345100@gmail.com>
2019-03-23 21:28:38 +0900

    Removed unusing code

--- src/module.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-function sum(value1: number, value2: number) {
-	return value1 + value2;
-}
-
-module.exports = {
-	sum
-};
--- src/module.test.js
+++
@@ -1,6 +0,0 @@
-/* @flow */
-const { sum } = require('./module');
-
-test('adds 1 + 2 to equal 3', () => {
-	expect(sum(1, 2)).toBe(3);
-});
