e75f1619de386e9d15b94057ceb41e8b014ac30a
modernator <rico345100@gmail.com>
2019-03-23 21:28:56 +0900

    Added Socket.io-file class and types

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 /* @flow */
+import type { IO, UploadSettings } from './types';
+
+class SocketIOFile {
+	/* Property type defs */
+	socket: IO;
+
+	constructor(socket: IO, uploadSettings: UploadSettings) {
+		this.socket = socket;
+
+		this.initEvents();
+	}
+
+	/**
+	 * Initialize Events to receive request from Client
+	 */
+	initEvents() {
+		this.socket.on('socket.io-file::upload', this.handleUpload);
+		this.socket.on('socket.io-file::uploadCancel', this.handleUploadCancel);
+	}
+
+	// TODO: Implement Upload sequences
+	handleUpload() {
+
+	}
+
+	// TODO: Implement Cancel sequences
+	handleUploadCancel(uploadId: number) {
+		
+	}
+}
+
+module.exports = SocketIOFile;
---
+++ src/types.js
@@ -0,0 +1,28 @@
+/* @flow */
+export type IO = any;
+
+export interface FileInfo {
+	originalFileName: string;
+	fileName: string;
+	fileSize: number;
+	mimeType: string;
+}
+
+export interface UploadInfo {
+	uploadId: number;
+	completed: boolean;
+	fileInfo: FileInfo;
+};
+
+export type DirectoryStore = {
+	[directoryName: string]: string;
+}
+
+export interface UploadSettings {
+	directory: string | DirectoryStore;
+	accepts?: string[];
+	maxFileSize?: number;
+	chunkSize?: number;
+	transmissionDelay?: number;
+	overwrite?: boolean;
+}
